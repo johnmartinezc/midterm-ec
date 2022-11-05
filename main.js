71d9d3e1b747034cf93b393788358c423add338b
johnmartinezc <john.martinez@codeimmersives.com>
2022-11-05 00:49:25 -0400

    -a

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-
-
-
- if (typeof twoSmallest === 'undefined') {
-    twoSmallest = undefined
+function twoSmallest(array){
+  let smallArr = []
+  let smallest = Math.min()
+  let secondSmallest = Math.min()
+  if(array.length < 2){
+    return undefined
   }
-
-
- module.exports = {
-    twoSmallest,
+  for (let num = 0; num < array.length; num++) {
+    if (smallest > array[num]) {
+        smallest = array[num]
+    }
+    }
+    for (let num = 0; num < array.length; num++) {
+   if (secondSmallest > array[num] && smallest < array[num]) {
+    secondSmallest = array[num]
   }
+}
+smallArr = [smallest , secondSmallest]
+  return smallArr
+  }
+   
+    
+
+
+if (typeof twoSmallest === 'undefined') {
+  twoSmallest = undefined
+}
+
+
+module.exports = {
+  twoSmallest,
+}
